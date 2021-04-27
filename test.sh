25b37394e281cc4eb6a9544094e91deed96a78fa
sushama5177573 <sp.5177573@gmail.com>
2021-04-27 23:29:37 +0530

    test.sh file is added

---
+++ test.sh
@@ -0,0 +1,3 @@
+ new file added to check whether below command is working for new file or not
+
+ #git commit -a -m "Updated" # normally this comman will add the existing updated file to stagging area & will commit in one go
